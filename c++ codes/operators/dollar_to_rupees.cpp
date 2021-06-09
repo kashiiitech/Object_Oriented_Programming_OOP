02a387e1d25c10dff5038d22f9c8d1ac4c8ccc37
Kashif Ali <kashiiitech@gmail.com>
2021-06-09 17:28:06 +0500

    It_will_take_temperature_in_centigrade_as_an_input_and_convert_it_into_fahrenheit

---
+++ c++ codes/operators/dollar_to_rupees.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int dollar,rupees=156;
+
+    cout<<"Enter the dollars: ";
+    cin>>dollar;
+    cout<<dollar<<" will be equal to "<<dollar*rupees<<" rupees."<<endl;
+
+    return 0;
+}
